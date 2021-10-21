0208aefb5335dfe0395cbf379be2c6c3576e48a5
naveen8801 <naveensharma10d@gmail.com>
2021-10-21 21:03:28 +0530

    basic code editor added

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@mui/styles';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
+import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
 
 const useStyles = makeStyles(() => ({
   editor: {
     width: '80%',
     height: '85vh',
-    backgroundColor: 'red',
+    // backgroundColor: 'red',
     display: 'flex',
     flexDirection: 'column',
   },
   toolbar: {
     width: '100%',
-    backgroundColor: 'green',
+    // backgroundColor: 'green',
     height: '80px',
     display: 'flex',
     alignItems: 'center',
   },
   textArea: {
     width: '100%',
-    backgroundColor: 'blue',
+    // backgroundColor: 'blue',
     height: '500px',
     marginTop: '1rem',
+    backgroundColor: 'white',
   },
   dropdown: {
-    backgroundColor: '#F94B25',
+    // backgroundColor: '#F94B25',
   },
 }));
 
 function CodeEditor() {
   const classes = useStyles();
+  //   const [code, setCode] = React.useState(
+  //     `function add(a, b) {\n  return a + b;\n}`
+  //   );
+  const [value, setValue] = useState('');
+  const handleSelect = (e) => {
+    console.log(e);
+    setValue(e);
+  };
+
   return (
     <div className={classes.editor}>
       <div className={classes.toolbar}>
@@ -37,14 +48,21 @@ function CodeEditor() {
           className={classes.dropdown}
           id="dropdown-basic-button"
           title="Language"
+          onSelect={handleSelect}
         >
-          <Dropdown.Item>Java</Dropdown.Item>
-          <Dropdown.Item>C++</Dropdown.Item>
-          <Dropdown.Item>Python</Dropdown.Item>
-          <Dropdown.Item>JavaScript</Dropdown.Item>
+          <Dropdown.Item eventKey="Java">Java</Dropdown.Item>
+          <Dropdown.Item eventKey="C++">C++</Dropdown.Item>
+          <Dropdown.Item eventKey="Python">Python</Dropdown.Item>
+          <Dropdown.Item eventKey="JavaScript">JavaScript</Dropdown.Item>
         </DropdownButton>
       </div>
-      <div className={classes.textArea}></div>
+      <div id="textarea" className={classes.textArea}>
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+        />
+      </div>
     </div>
   );
 }
